743a60ffc32fbb984e47621daa3f508e97058959
Mohin Paramasivam <mohin2099@gmail.com>
2021-03-24 00:07:56 +0800

    Update nipper_parser.py

--- nipper_parser.py
+++ nipper_parser.py
@@ -1,11 +1,33 @@
 from bs4 import BeautifulSoup as bs
-import time
 import xlwt 
 from xlwt import Workbook 
 
+
+
+
+
+
+print ("                                                                       ")
+print ("                                                                       ")
+print ("            #####################################################" )       
+print ("            #                                                   #" )        
+print ("            #              Nipper XML Report Parser             #" ) 
+print ("            #                                                   #" )                            
+print ("            #            Author : Mohin Paramasivam             #" )       
+print ("            #                                                   #" )       
+print ("            #####################################################" )    
+
+
 content = []
 # Read the XML file
-with open("seremban.xml", "r") as file:
+
+print("\r\n")
+print("   Convert HTML Report file to XML at https://www.freefileconvert.com\r\n\r\n")
+read_file_xml = input("Nipper XML Input File : ")
+print("\r\n")
+output_file_excel = input("Enter Output Filename with '.xls' extension : ")
+
+with open(read_file_xml, "r") as file:
     # Read each line in the file, readlines() returns a list of lines
     content = file.readlines()
     # Combine the lines in the list into a string
@@ -17,9 +39,10 @@ find_anchor_counter = 0
 
 found_anchor = False
 
-print("\n")
-sect_start_input = int(input("Enter start <sect1 location number : ")) # find the start <sect1
-sect_end_input = int(input("Enter <sect1 location number : ")) # find the end <sect1
+print("\r\n")
+sect_start_input = int(input("Enter start <sect1> location number : ")) # find the start <sect1>
+print("\r\n")
+sect_end_input = int(input("Enter end <sect1> location number : ")) # find the end <sect1>
 
 
 sect_start = sect_start_input-1
@@ -34,7 +57,7 @@ sheet1 = wb.add_sheet('Sheet 1')
 
 # While there is more data in the xml file continue looping
 while(sect_start!=sect_end):
-	
+			
 	
 	#If the column reaches 3 then reset column and row coordinates
 	
@@ -42,31 +65,28 @@ while(sect_start!=sect_end):
 		section_part_column=0
 		section_part_row+=1
 		content = bs_content.findAll('sect1')[sect_start].text
-		print("Writing location : Row --> %s Column--> %s " %(section_part_row,section_part_column))
+		print("[+] Writing location : Row --> %s Column--> %s " %(section_part_row,section_part_column))
 		sheet1.write(section_part_row,section_part_column,content)
-		wb.save('test.xls')
+		wb.save(output_file_excel)
 		section_part_column+=1
 		
 		
 	else:
 	
 		content = bs_content.findAll('sect1')[sect_start].text
-		print("Writing location : Row --> %s Column--> %s " %(section_part_row,section_part_column))
+		print("[+] Writing location : Row --> %s Column--> %s " %(section_part_row,section_part_column))
 		sheet1.write(section_part_row, section_part_column,content)
-		wb.save('test.xls')
+		wb.save(output_file_excel)
 		section_part_column+=1
 		
 		
 		
-	#Counter for sect incement
+	#Counter for sect increment
 	sect_start+=1
 	
-			
-			
-			
-			
-	
-		
+print("\r\n")
+print("[+] Successfully Written to --> %s" %(output_file_excel))	
+print("\r\n")	
 		
 		
 		
